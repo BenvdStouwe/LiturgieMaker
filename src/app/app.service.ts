3514beb9fdc6e6dc650bcb4dc143290ac7a385c7
BenvdStouwe <benvdstouwe@gmail.com>
2018-01-15 09:25:33 +0100

    Header & footer dingen

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,8 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppService {
   private title = 'LiturgieMaker';
+  private startingYear = 2018;
 
   public getTitle(): string {
     return this.title;
+  }
+
+  public getStartingYear(): number {
+    return this.startingYear;
   }
 }
--- src/app/basicComponents/footer.component.ts
+++ src/app/basicComponents/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppService } from '../app.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: 'footer.component.html',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class FooterComponent implements OnInit {
-  constructor() { }
+  private title: string;
+  private startingYear: number;
+  private currentYear: number;
 
-  ngOnInit() { }
+  constructor(private appService: AppService) { }
+
+  ngOnInit() {
+    this.title = this.appService.getTitle();
+    this.startingYear = this.appService.getStartingYear();
+    this.currentYear = new Date().getFullYear();
+  }
+
+  private getYears(): string {
+    return this.startingYear === this.currentYear
+      ? this.startingYear.toString()
+      : this.startingYear.toString() + ' - ' + this.currentYear.toString();
+  }
 }
